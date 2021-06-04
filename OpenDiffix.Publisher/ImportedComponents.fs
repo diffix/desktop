ca46593c6c6a19af0f39de94b7482deacc0b2947
Sebastian Probst Eide <sebastian@aircloak.com>
2021-06-04 13:12:32 +0200

    Move CsvDropzone to components folder

--- OpenDiffix.Publisher/ImportedComponents.fs
+++ OpenDiffix.Publisher/ImportedComponents.fs
@@ -2,7 +2,7 @@ module OpenDiffix.Publisher.ImportedComponents
 
 open Feliz
 
-[<ReactComponent(import="CsvDropzone", from="../src/renderer/CsvDropzone.js")>]
+[<ReactComponent(import="CsvDropzone", from="../src/renderer/components/CsvDropzone.js")>]
 let CsvDropzone (loadCsv: string -> unit) = React.imported()
 
 [<ReactComponent(import="Toggle", from="../src/renderer/components/Toggle.js")>]
