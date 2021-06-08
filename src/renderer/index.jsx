965d31cc624c919ac6ff706214c2f708bef61774
Edon Gashi <egashi@mpi-sws.org>
2021-06-08 14:04:28 +0200

    Set up electron TS react template

--- src/renderer/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./index.css";
-
-import { App } from '../compiled/App.js'
-
-ReactDOM.render(<App />, document.getElementById("root"));
