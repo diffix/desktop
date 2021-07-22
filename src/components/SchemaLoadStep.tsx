58ad1f1efc70cc2bd2aeba057f9652ae897e007d
Edon Gashi <egashi@mpi-sws.org>
2021-07-22 17:46:00 +0200

    Convert schema loader to notebook step

--- src/components/SchemaLoadStep.tsx
+++ src/components/SchemaLoadStep.tsx
@@ -1,20 +1,23 @@
 import React, { FunctionComponent, useEffect } from 'react';
-import { Button, message, Result, Space, Spin, Typography } from 'antd';
+import { message, Result, Space, Spin, Typography } from 'antd';
 
 import { useSchema } from '../state';
 import { File, TableSchema } from '../types';
 
-import './SchemaLoader.css';
+import './SchemaLoadStep.css';
 
 const { Text } = Typography;
 
-export type SchemaLoaderProps = {
+export type SchemaLoadStepProps = {
   file: File;
-  removeFile: () => void;
-  children: (schema: TableSchema) => React.ReactNode;
+  children: (data: SchemaLoadStepData) => React.ReactNode;
 };
 
-export const SchemaLoader: FunctionComponent<SchemaLoaderProps> = ({ children, file, removeFile }) => {
+export type SchemaLoadStepData = {
+  schema: TableSchema;
+};
+
+export const SchemaLoadStep: FunctionComponent<SchemaLoadStepProps> = ({ children, file }) => {
   const schema = useSchema(file.path);
   useEffect(() => {
     if (schema.state === 'completed') {
@@ -24,33 +27,33 @@ export const SchemaLoader: FunctionComponent<SchemaLoaderProps> = ({ children, f
 
   switch (schema.state) {
     case 'completed':
-      return <div className="SchemaLoader completed">{children(schema.value)}</div>;
+      return (
+        <>
+          <div className="SchemaLoadStep notebook-step completed">
+            <h2>{file.name}</h2>
+          </div>
+          {/* Render next step */}
+          {children({ schema: schema.value })}
+        </>
+      );
 
     case 'failed':
       return (
-        <div className="SchemaLoader failed">
+        <div className="SchemaLoadStep notebook-step failed">
           <Result
             status="error"
             title="Schema discovery failed"
             subTitle="Something went wrong while loading the schema."
-            extra={
-              <Button type="primary" onClick={removeFile}>
-                Choose another file
-              </Button>
-            }
           />
         </div>
       );
 
     case 'in_progress':
       return (
-        <div className="SchemaLoader loading">
+        <div className="SchemaLoadStep notebook-step loading">
           <Space direction="vertical">
             <Spin size="large" />
             <Text type="secondary">Loading schema</Text>
-            <Button type="ghost" onClick={removeFile}>
-              Cancel
-            </Button>
           </Space>
         </div>
       );
