3b968ccf7743d5da0485d4c296840849de370f87
Edon Gashi <egashi@mpi-sws.org>
2021-07-22 18:52:45 +0200

    Convert anonymization to notebook step

---
+++ src/components/AnonymizationStep.tsx
@@ -0,0 +1,40 @@
+import React, { FunctionComponent } from 'react';
+import { Button, Result } from 'antd';
+
+import { QueryResultsTable } from '.';
+import { useCachedData, useQuery } from '../state';
+import { QueryResult, TableSchema } from '../types';
+
+const emptyQueryResult: QueryResult = { columns: [], rows: [] };
+
+export type AnonymizationStepProps = {
+  columns: boolean[];
+  schema: TableSchema;
+};
+
+export const AnonymizationStep: FunctionComponent<AnonymizationStepProps> = ({ columns, schema }) => {
+  const computedResult = useQuery(schema, columns);
+  const cachedResult = useCachedData(computedResult, emptyQueryResult);
+
+  switch (computedResult.state) {
+    case 'in_progress':
+    case 'completed': {
+      const loaded = computedResult.state === 'completed';
+      return (
+        <div className="AnonymizationStep notebook-step completed">
+          <QueryResultsTable loading={!loaded} result={cachedResult} />
+          <Button type="primary" block disabled={!loaded}>
+            Export to CSV
+          </Button>
+        </div>
+      );
+    }
+
+    case 'failed':
+      return (
+        <div className="AnonymizationStep notebook-step failed">
+          <Result status="error" title="Anonymization failed" subTitle="Something went wrong." />
+        </div>
+      );
+  }
+};
