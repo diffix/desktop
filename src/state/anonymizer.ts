136f8452fc5d5aae3d1b221a0e19616b65ae7325
Cristian Berneanu <bcristi@gmail.com>
2021-07-28 09:58:31 +0300

    Detect column types from the rows preview.

--- src/state/anonymizer.ts
+++ src/state/anonymizer.ts
@@ -4,6 +4,7 @@ import {
   AnonymizedResultColumn,
   AnonymizedResultRow,
   Anonymizer,
+  ColumnType,
   File,
   TableColumn,
   TableSchema,
@@ -17,17 +18,52 @@ class DiffixAnonymizer implements Anonymizer {
     return BigInt('0x' + hash.substring(0, 16)).toString(10); // Return a 64-bit decimal string.
   }
 
+  private static booleanRE = /^(?:true|false|0|1)$/i;
+  private static integerRE = /^-?\d{1,20}$/;
+  private static realRE = /^-?\d{1,20}(?:\.\d{1,20})?$/;
+
+  private detectColumnTypes(columnsCount: number, rows: string[][]): ColumnType[] {
+    const typesInfo = Array(columnsCount)
+      .fill(null)
+      .map(() => ({ isEmpty: true, isBoolean: true, isInteger: true, isReal: true }));
+
+    for (const row of rows) {
+      for (let index = 0; index < row.length; index++) {
+        const value = row[index];
+        if (value) {
+          // Not null and not empty.
+          const typeInfo = typesInfo[index];
+          typeInfo.isEmpty = false;
+          typeInfo.isBoolean &&= DiffixAnonymizer.booleanRE.test(value);
+          typeInfo.isInteger &&= DiffixAnonymizer.integerRE.test(value);
+          typeInfo.isReal &&= DiffixAnonymizer.realRE.test(value);
+        }
+      }
+    }
+
+    return typesInfo.map((info) => {
+      if (info.isEmpty) return 'string';
+      if (info.isBoolean) return 'boolean';
+      if (info.isInteger) return 'integer';
+      if (info.isReal) return 'real';
+      return 'string';
+    });
+  }
+
   loadSchema(file: File): Task<TableSchema> {
     return toTask(async () => {
       const fileName = file.path;
       const result = await window.executeQuery(fileName, '0', 'SELECT * FROM table LIMIT 10000');
       const salt = await this.computeSaltFromFile(fileName);
-      return {
-        file,
-        columns: result.columns.slice(1), // Drop row index column from schema.
-        rowsPreview: result.rows.map((row) => row.slice(1)),
-        salt,
-      };
+
+      // Drop row index column from schema.
+      const columns = result.columns.slice(1);
+      const rowsPreview = result.rows.map((row) => row.slice(1));
+
+      const types = this.detectColumnTypes(columns.length, rowsPreview as string[][]);
+      for (let index = 0; index < columns.length; index++) columns[index].type = types[index];
+
+      return { file, columns, rowsPreview, salt };
     });
   }
 
