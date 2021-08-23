f5c0a5041e98df62b3cd2a3a687a103e645dd52c
Edon Gashi <egashi@mpi-sws.org>
2021-08-23 14:16:50 +0200

    Add useMemoStable hook

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
+import { isEqual } from 'lodash';
 import { ComputedData } from '../types';
 
 export function useCachedData<T>(resultData: ComputedData<T>, initialData: T): T {
@@ -9,4 +10,20 @@ export function useCachedData<T>(resultData: ComputedData<T>, initialData: T): T
   }
 
   return ref.current;
+}
+
+/** Similar to `useMemo`, but retains reference for deep-equal values. */
+export function useMemoStable<T>(factory: () => T, deps: React.DependencyList): T {
+  const ref = useRef<T>();
+
+  return useMemo(() => {
+    const value = factory();
+    if (isEqual(value, ref.current)) {
+      return ref.current as T;
+    }
+
+    ref.current = value;
+    return value;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
 }
