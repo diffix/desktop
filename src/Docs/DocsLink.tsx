4ba1efe4ae0a038dcbae59319b0516f8b07bd83a
Edon Gashi <egashi@mpi-sws.org>
2021-10-11 16:59:53 +0200

    Rewrite markdown links

--- src/Docs/DocsLink.tsx
+++ src/Docs/DocsLink.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useContext } from 'react';
 import { Typography } from 'antd';
 import { noop } from 'lodash';
+import classNames from 'classnames';
 
 import { PageId } from './Docs';
 
@@ -14,17 +15,23 @@ export const DocsFunctionsContext = React.createContext<DocsFunctions>({
   openDocs: noop,
 });
 
+function placeholderHref(page: string, section: string | null | undefined) {
+  if (section) return `/${page}.md#${section}`;
+  else return `/${page}.md`;
+}
+
 export type DocsLinkProps = {
+  className?: string;
   page: PageId;
   section?: string | null;
 };
 
-export const DocsLink: FunctionComponent<DocsLinkProps> = ({ page, section, children }) => {
+export const DocsLink: FunctionComponent<DocsLinkProps> = ({ className, page, section, children }) => {
   const docsFunctions = useContext(DocsFunctionsContext);
   return (
     <Link
-      className="DocsLink"
-      href="#"
+      className={classNames('DocsLink', className)}
+      href={placeholderHref(page, section)}
       onClick={(e) => {
         e.preventDefault();
         docsFunctions.openDocs(page, section ?? null);
